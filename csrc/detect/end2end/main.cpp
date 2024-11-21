68411126be5a8e0caed62a6fa7268874fb825dcf
Ratan Othayoth <othayothr@janelia.hhmi.org>
2024-11-21 14:50:01 -0500

    building locally for mouse

--- csrc/detect/end2end/main.cpp
+++ csrc/detect/end2end/main.cpp
@@ -6,39 +6,18 @@
 #include "yolov8.hpp"
 
 const std::vector<std::string> CLASS_NAMES = {
-    "person",         "bicycle",    "car",           "motorcycle",    "airplane",     "bus",           "train",
-    "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",    "parking meter", "bench",
-    "bird",           "cat",        "dog",           "horse",         "sheep",        "cow",           "elephant",
-    "bear",           "zebra",      "giraffe",       "backpack",      "umbrella",     "handbag",       "tie",
-    "suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball",  "kite",          "baseball bat",
-    "baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",       "wine glass",    "cup",
-    "fork",           "knife",      "spoon",         "bowl",          "banana",       "apple",         "sandwich",
-    "orange",         "broccoli",   "carrot",        "hot dog",       "pizza",        "donut",         "cake",
-    "chair",          "couch",      "potted plant",  "bed",           "dining table", "toilet",        "tv",
-    "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",   "microwave",     "oven",
-    "toaster",        "sink",       "refrigerator",  "book",          "clock",        "vase",          "scissors",
-    "teddy bear",     "hair drier", "toothbrush"};
+   "mouse", "ball"};
 
 const std::vector<std::vector<unsigned int>> COLORS = {
-    {0, 114, 189},   {217, 83, 25},   {237, 177, 32},  {126, 47, 142},  {119, 172, 48},  {77, 190, 238},
-    {162, 20, 47},   {76, 76, 76},    {153, 153, 153}, {255, 0, 0},     {255, 128, 0},   {191, 191, 0},
-    {0, 255, 0},     {0, 0, 255},     {170, 0, 255},   {85, 85, 0},     {85, 170, 0},    {85, 255, 0},
-    {170, 85, 0},    {170, 170, 0},   {170, 255, 0},   {255, 85, 0},    {255, 170, 0},   {255, 255, 0},
-    {0, 85, 128},    {0, 170, 128},   {0, 255, 128},   {85, 0, 128},    {85, 85, 128},   {85, 170, 128},
-    {85, 255, 128},  {170, 0, 128},   {170, 85, 128},  {170, 170, 128}, {170, 255, 128}, {255, 0, 128},
-    {255, 85, 128},  {255, 170, 128}, {255, 255, 128}, {0, 85, 255},    {0, 170, 255},   {0, 255, 255},
-    {85, 0, 255},    {85, 85, 255},   {85, 170, 255},  {85, 255, 255},  {170, 0, 255},   {170, 85, 255},
-    {170, 170, 255}, {170, 255, 255}, {255, 0, 255},   {255, 85, 255},  {255, 170, 255}, {85, 0, 0},
-    {128, 0, 0},     {170, 0, 0},     {212, 0, 0},     {255, 0, 0},     {0, 43, 0},      {0, 85, 0},
-    {0, 128, 0},     {0, 170, 0},     {0, 212, 0},     {0, 255, 0},     {0, 0, 43},      {0, 0, 85},
-    {0, 0, 128},     {0, 0, 170},     {0, 0, 212},     {0, 0, 255},     {0, 0, 0},       {36, 36, 36},
-    {73, 73, 73},    {109, 109, 109}, {146, 146, 146}, {182, 182, 182}, {219, 219, 219}, {0, 114, 189},
-    {80, 183, 189},  {128, 128, 0}};
+    {0, 114, 189},   {217, 83, 25}};
 
 int main(int argc, char** argv)
 {
     // cuda:0
     cudaSetDevice(0);
+
+    std::string buildInfo = cv::getBuildInformation();
+    std::cout << buildInfo << std::endl;
 
     const std::string engine_file_path{argv[1]};
     const std::string path{argv[2]};
@@ -51,7 +30,8 @@ int main(int argc, char** argv)
     auto yolov8 = new YOLOv8(engine_file_path);
     yolov8->make_pipe(true);
 
-    if (IsFile(path)) {
+    if (IsFile(path)) 
+    {
         std::string suffix = path.substr(path.find_last_of('.') + 1);
         if (suffix == "jpg" || suffix == "jpeg" || suffix == "png") {
             imagePathList.push_back(path);
@@ -73,11 +53,19 @@ int main(int argc, char** argv)
     cv::Size            size = cv::Size{640, 640};
     std::vector<Object> objs;
 
-    cv::namedWindow("result", cv::WINDOW_AUTOSIZE);
+    
+    cv::namedWindow("result", cv::WINDOW_NORMAL);
+    cv::resizeWindow("result", 800, 2200/4);
 
     if (isVideo) {
         cv::VideoCapture cap(path);
-
+        std::cout  << "format " << cap.get(cv::CAP_PROP_FRAME_WIDTH) <<std::endl;
+        if (!IsFile(path)) {
+            printf("Video %s does not exist\n", path.c_str());
+            return -1;
+        }
+        else
+            printf("file exists\n");
         if (!cap.isOpened()) {
             printf("can not open %s\n", path.c_str());
             return -1;
